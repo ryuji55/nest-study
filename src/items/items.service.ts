dad6af5227f23472681cd7a416c93fe81b50dc1e
ryuji55 <rt.october.2@icloud.com>
2023-12-04 08:24:58 +0900

    Fix

---
+++ src/items/items.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { Item } from './items.model';
+
+@Injectable()
+export class ItemsService {
+  private items: Item[] = [];
+  findAll(): Item[] {
+    return this.items;
+  }
+
+  create(item: Item): Item {
+    this.items.push(item);
+    return item;
+  }
+}
